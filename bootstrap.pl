aafaab7e9b4ba3981dca166f3fff5a2efee64b61
Yutaka Ichibangase <yichiban@gmail.com>
2021-12-05 10:32:49 +0900

    fix member/2.

--- bootstrap.pl
+++ bootstrap.pl
@@ -216,7 +216,7 @@ append([], L, L).
 append([X|L1], L2, [X|L3]) :- append(L1, L2, L3).
 
 :- built_in(member/2).
-member(X, [X]).
+member(X, [X|_]).
 member(X, [_|Xs]) :- member(X, Xs).
 
 :- built_in(length/2).
