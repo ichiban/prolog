d95037618878d120a6468a2a78898339f46f90c5
Yutaka Ichibangase <yichiban@gmail.com>
2022-03-22 19:50:09 +0900

    add succ/2

--- bootstrap.pl
+++ bootstrap.pl
@@ -250,7 +250,7 @@ maplist(Cont, [E|Es]) :-
 :- built_in(phrase/2).
 phrase(GRBody, S0) :- phrase(GRBody, S0, []).
 
-:-built_in(select/3).
+:- built_in(select/3).
 select(E, [E|Xs], Xs).
 select(E, [X|Xs], [X|Ys]) :-
    select(E, Xs, Ys).
