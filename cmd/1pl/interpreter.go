da30f61a11dd91e458affb2e6f3c35184e83b604
Yutaka Ichibangase <yichiban@gmail.com>
2022-10-23 11:26:56 +0900

    remove an unused predicate

--- cmd/1pl/interpreter.go
+++ cmd/1pl/interpreter.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"time"
-
 	"github.com/ichiban/prolog"
 	"github.com/ichiban/prolog/engine"
+	"io"
 )
 
 // New creates a prolog.Interpreter with some helper predicates.
@@ -17,14 +15,6 @@ func New(r io.Reader, w io.Writer) *prolog.Interpreter {
 	i.Register2("go_string", func(term, s engine.Term, k func(*engine.Env) *engine.Promise, env *engine.Env) *engine.Promise {
 		engine.GoStringEnv = env
 		return engine.Unify(s, engine.Atom(fmt.Sprintf("%#v", term)), k, env)
-	})
-	i.Register2("time", func(goal, out engine.Term, k func(*engine.Env) *engine.Promise, env *engine.Env) *engine.Promise {
-		start := time.Now()
-		return i.Call(goal, func(env *engine.Env) *engine.Promise {
-			end := time.Now()
-			d := end.Sub(start)
-			return engine.Unify(out, engine.Atom(d.String()), k, env)
-		}, env)
 	})
 	return i
 }
